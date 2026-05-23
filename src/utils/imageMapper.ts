d6f88c3b25c2fa64178f5ee97bea90971f143c73
Ezana Tadesse <ezanatadesse2003@gmail.com>
2026-05-24 01:06:30 +0300

    Added receipt image mapper

--- src/utils/imageMapper.ts
+++ src/utils/imageMapper.ts
@@ -13,3 +13,10 @@ export const mapProductImage = (image: any): ProductImageResult => {
         image_url: buildImageUrl(image.image_url)
     };
 };
+
+export const mapReceiptImage = (url: any): { image_name: string, image_url: string } => {
+     return {
+        image_name: url,
+        image_url: buildImageUrl(url)
+    };
+};
