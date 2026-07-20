9645a9c4b2a461afdc615c72a920bfa41afaa765
Ezana Tadesse <ezanatadesse2003@gmail.com>
2026-07-20 15:16:15 +0300

    Added a route, controller and service to retrieve list of orders only belonging to the authenticated user.

--- src/routes/order.routes.ts
+++ src/routes/order.routes.ts
@@ -3,16 +3,18 @@ import { authenticate } from "../middleware/authenticator";
 import { checkOwnershipOrAdmin } from "../middleware/ownershipOrAdminChecker";
 import { validate } from "../middleware/validator";
 import { createOrderSchema, getOrderSchema, getOrdersForAdminSchema, updateOrderSchema } from "../validators/order.validator";
-import { createOrder, getOrder, getOrders, updateOrder } from "../controllers/order.controller";
+import { createOrder, getMyOrders, getOrder, getOrders, updateOrder } from "../controllers/order.controller";
 
 const router = Router();
 
 router.post('/', authenticate, validate({body: createOrderSchema}), createOrder);
 
+router.get('/my', authenticate, validate({query: getOrdersForAdminSchema}), getMyOrders);
 router.get('/:id', authenticate, validate({params: getOrderSchema}), getOrder);
 router.get('/', authenticate, validate({query: getOrdersForAdminSchema}), getOrders);
 
 router.put('/:id', authenticate, validate({params: getOrderSchema, body: updateOrderSchema}), updateOrder);
+
 
 
 export default router;
